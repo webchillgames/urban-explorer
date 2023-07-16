8b5747359bd7fc23111d999a7c5ee40e91a04f4c
webchillgames <web.chill.games@gmail.com>
2023-07-16 11:32:51 +0800

    draft

--- src/components/AframeScene.vue
+++ src/components/AframeScene.vue
@@ -44,53 +44,58 @@ export default defineComponent({
   setup(props, ctx) {
     const items = computed(() => props.items)
     const reminder = computed(() => items.value.filter((v: IItem) => v.isCatched === false))
+    
+    function createModelElement(item: IItem) {
+      console.log(555, item, item.id);
+      
+      const modelEl = document.createElement('a-gltf-model')
+      const modelWrapper = document.createElement('a-entity')
+      modelEl.setAttribute('model', '')
+      modelEl.setAttribute('id', `${item.id}`)
+      modelEl.setAttribute('src', `${item.model}/scene.gltf`)
+      modelEl.classList.add('clickable')
+      modelWrapper.setAttribute(
+        'gps-new-entity-place',
+        `latitude: ${item.coords[0]}; longitude: ${item.coords[1]};`
+      )
+
+      if (item.scale) {
+        modelEl.setAttribute('scale', item.scale)
+      } else {
+        modelEl.setAttribute('scale', '1 1 1')
+      }
+
+      if (item.rotation) {
+        modelEl.setAttribute('rotation-component', '')
+        modelEl.setAttribute('rotation', item.rotation)
+      } else {
+        modelEl.setAttribute('rotation', '0 0 0')
+        // modelEl.setAttribute('look-at', '#camera')
+      }
+
+      if (item.position) {
+        modelEl.setAttribute('position', item.position)
+      } else {
+        modelEl.setAttribute('position', '0 0 0')
+      }
+
+      modelWrapper.appendChild(modelEl)
+
+      return modelWrapper
+    }
 
     AFRAME.registerComponent('scene', {
       init() {
-        // alert('началось')
         items.value.forEach((item: IItem) => {
-          if (item.isShow) {
-            const modelEl = document.createElement('a-gltf-model')
-            const parentToRotate = document.createElement('a-entity')
-            modelEl.setAttribute('model', '')
-            modelEl.setAttribute('id', `${item.id}`)
-            modelEl.setAttribute('src', `${item.model}/scene.gltf`)
-            parentToRotate.setAttribute(
-              'gps-new-entity-place',
-              `latitude: ${item.coords[0]}; longitude: ${item.coords[1]};`
-            )
-            modelEl.classList.add('clickable')
-
-            if (item.scale) {
-              modelEl.setAttribute('scale', item.scale)
-            } else {
-              modelEl.setAttribute('scale', '1 1 1')
-            }
-
-            if (item.rotation) {
-              console.log(item.id, item.rotation)
-              modelEl.setAttribute('rotation-component', '')
-              modelEl.setAttribute('rotation', item.rotation)
-            } else {
-              modelEl.setAttribute('rotation', '0 0 0')
-              // modelEl.setAttribute('look-at', '#camera')
-            }
-
-            if (item.position) {
-              modelEl.setAttribute('position', item.position)
-            }
-            // modelEl.setAttribute('animation-mixer', '')
-            parentToRotate.appendChild(modelEl)
-            this.el.appendChild(parentToRotate)
+          if (item.isShow) {           
+            this.el.appendChild(createModelElement(item))
           }
-
-          // alert('Подгрузилась')
         })
       }
     })
 
     AFRAME.registerComponent('user-raycaster', {
-      dependencies: ['raycaster']
+      dependencies: ['raycaster'],
       // init() {
       //   this.el.addEventListener('raycaster-intersection', function (evt) {
       //     console.log('raycaster-intersection')
@@ -100,18 +105,6 @@ export default defineComponent({
       //     this.raycaster = null
       //   })
       // },
-
-      // tick() {
-      //   if (!this.raycaster) {
-      //     return
-      //   } // Not intersecting.
-      //   let intersection = this.raycaster.components.raycaster.getIntersection(this.el)
-      //   if (!intersection) {
-      //     return
-      //   } // Not intersecting
-      //   // intersecting
-      //   console.log(intersection)
-      // }
     })
 
     AFRAME.registerComponent('model', {
@@ -119,8 +112,6 @@ export default defineComponent({
         const scene = document.querySelector('a-scene')
         this.el.addEventListener('raycaster-intersected', (evt: any) => {
           const id = evt.target.getAttribute('id')
-          console.log(6666666);
-          
           // evt.target.setAttribute('animation', 'property: scale; to: 0; loop: false; dur: 1000')
 
           if (!reminder.value.length) {
@@ -128,18 +119,20 @@ export default defineComponent({
               ctx.emit('finish')
             }, 2000)
           }
+console.log(evt.target);
 
           return items.value.forEach((item) => {
-            if (item.id === Number(id)) {
+            if (item.id === id) {
               if (item.isCatched) {
                 return
               } else {
+                console.log('catched');
+                
                 item.isCatched = true
                 const el = document.getElementById(`${item.id}`)
 
                 if (scene && el) {
                   setTimeout(() => {
-
                     ctx.emit('catchItem', reminder.value.length)
                     scene.removeChild(el)
                   }, 2000)
@@ -154,18 +147,9 @@ export default defineComponent({
     AFRAME.registerComponent('user-camera', {
       init() {
         const raycaster = document.createElement('a-entity')
-        // const svg = document.createElement('a-plane')
-        // svg.setAttribute('geometry', 'primitive: plane; width: 1; height: 1')
-        // svg.setAttribute('material', 'src: url(/ilnur.jpg)')
-
         raycaster.setAttribute('user-raycaster', '')
         raycaster.setAttribute('raycaster', 'objects: .clickable')
         raycaster.setAttribute('cursor', 'fuse: true;')
-        // raycaster.setAttribute('material', 'color: orange; shader: flat')
-        raycaster.setAttribute('position', '0 0 -2')
-
-        // raycaster.appendChild(svg)
-        // raycaster.setAttribute('geometry', 'primitive: ring;radiusInner: 0.16; radiusOuter: 0.24;')
         this.el.appendChild(raycaster)
 
         // this.el.addEventListener('gps-camera-update-positon', (evt) => {
